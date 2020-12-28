05ca6aec879dfca40451784bef7baa4c2b568b6d
mari <bowman.marius@gmail.com>
2020-12-28 16:45:31 +0100

    removed comments

--- sf_crime_statistics/consumer_server.py
+++ sf_crime_statistics/consumer_server.py
@@ -6,10 +6,7 @@ def listen():
                              bootstrap_servers=["localhost:9092"],
                              client_id="sf-crime-consumer"
                              )
-    # print(f"subscribed to: {consumer.subscription()}")
     for message in consumer:
-        # message value and key are raw bytes -- decode if necessary!
-        # e.g., for unicode: `message.value.decode('utf-8')`
         print(f"{message.topic}:{message.offset}:\nkey={message.key} value={message.value}")
 
 
